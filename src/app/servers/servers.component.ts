bf97b956e1e15c93d5da631659f7d2b7b6fa2855
AkosNyiry <113112440+AkosNyiry@users.noreply.github.com>
2023-06-09 12:24:00 +0200

    2way databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { ServerCopmonent } from '../server/server.component';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout( () => {
@@ -20,6 +21,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = `A server with the name: ${this.serverName} was created!` ;
+  }
+
+  onUpdateServerName(event:any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value
   }
 }
